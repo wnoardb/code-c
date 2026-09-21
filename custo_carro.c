23319475a0da86cb5497d503d067f3681c2eb0be
FELIPE SANTANA DE ALMEIDA <2026221190@fmu.interno>
2026-09-21 11:29:54 -0300

    codigos

--- custo_carro.c
+++ custo_carro.c
@@ -2,17 +2,26 @@
 
 int main()
 {
-    float custoFabrica, distribuidor, impostos, custoFinal;
-
-    printf("Digite o custo de fabrica do carro: R$ ");
-    scanf("%f", &custoFabrica);
-
-    distribuidor = custoFabrica * 28 / 100;
-    impostos = custoFabrica * 45 / 100;
-
-    custoFinal = custoFabrica + distribuidor + impostos;
-
-    printf("Custo final do carro: R$ %.2f\n", custoFinal);
-
-    return 0;
+   float custoCarronovo;
+   float percDistribuidor=0.28; //28%
+   float percImposto= 0.45; //45%
+   float custoFabrica;
+   
+   printf("Digite o custo fábrica do carro: ");
+   scanf("%f",&custoFabrica);
+   
+   custoCarronovo=custoFabrica+(custoFabrica*percDistribuidor)+
+   (custoFabrica*percImposto);
+   
+   percDistribuidor=percDistribuidor*custoFabrica;
+   
+   percImposto=percImposto*custoFabrica;
+   
+   printf("Custo do carro novo: %.2f\n", custoCarronovo);
+   printf("Valor distribuidor: %.2f\n",percDistribuidor);
+   printf("Valor imposto: %.2f\n",percImposto);
+   printf("Valor de fábrica: %.2f",custoFabrica);
+   
+   return 0;
+   
 }
